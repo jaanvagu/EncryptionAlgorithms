df13785cb072ee46ac82aaeb7335ed0483e50339
jaanvagu <jaanvagu@gmail.com>
2019-05-01 19:31:02 -0500

    Adición de clase inicial principal

---
+++ src/main/EncryptionAlgorithms.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Arrays;
+
+public class EncryptionAlgorithms {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
