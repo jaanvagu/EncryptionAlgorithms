6c1032d064b9242c55bd00db0008d1120a71118f
jaanvagu <jaanvagu@gmail.com>
2019-05-01 21:58:44 -0500

    Base para lectura de parámetros y uso de paquetes

--- src/main/EncryptionAlgorithms.java
+++ src/main/EncryptionAlgorithms.java
@@ -1,12 +1,19 @@
 package main;
 
+import utils.Constants;
+
 import java.util.Arrays;
 
 public class EncryptionAlgorithms {
+
+    public static void imprimir(){
+        System.out.println("Imprimir");
+    }
 
     public static void main(String[] args) {
         for (String arg : args) {
             System.out.println(arg);
         }
+        System.out.println(Constants.prueba);
     }
 }
---
+++ src/utils/Constants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Constants {
+
+    public static final String prueba = "texto";
+}
